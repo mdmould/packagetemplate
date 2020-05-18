9ad4992bbf45662b23966439b985a0a5186ab5ac
MDM988 <mmould@star.sr.bham.ac.uk>
2020-05-18 21:39:11 +0100

    .

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,16 @@ sys.path.insert(0, os.path.abspath('../packagetemplate'))
 
 # -- Project information -----------------------------------------------------
 
+import _version as packagetemplate_version
+
 project = 'packagetemplate'
 copyright = 'Copyright (c) 2020 Matthew Mould'
 author = 'Matthew Mould'
+
+# The short X.Y version
+version = packagetemplate_version.__version__
+# The full version, including alpha/beta/rc tags
+release = version
 
 
 # -- General configuration ---------------------------------------------------
@@ -42,7 +49,7 @@ extensions = [
     # 'sphinx.ext.coverage',
     # 'sphinx.ext.mathjax',
     # 'sphinx.ext.viewcode',
-    # 'sphinx.ext.githubpages',
+    'sphinx.ext.githubpages',
     'recommonmark'
 ]
 
@@ -104,3 +111,88 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Custom sidebar templates, must be a dictionary that maps document names
+# to template names.
+#
+# The default sidebars (for documents that don't match any pattern) are
+# defined by theme itself.  Builtin themes are using these templates by
+# default: ``['localtoc.html', 'relations.html', 'sourcelink.html',
+# 'searchbox.html']``.
+#
+# html_sidebars = {}
+
+
+# -- Options for HTMLHelp output ---------------------------------------------
+
+# Output file base name for HTML help builder.
+htmlhelp_basename = 'packagetemplatedoc'
+
+
+# -- Options for LaTeX output ------------------------------------------------
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title,
+#  author, documentclass [howto, manual, or own class]).
+latex_documents = [
+    (master_doc, 'packagetemplate.tex', 'packagetemplate Documentation',
+     'Matthew Mould', 'manual'),
+]
+
+
+# -- Options for manual page output ------------------------------------------
+
+# One entry per manual page. List of tuples
+# (source start file, name, description, authors, manual section).
+man_pages = [
+    (master_doc, 'packagetemplate', 'packagetemplate Documentation',
+     [author], 1)
+]
+
+
+# -- Options for Texinfo output ----------------------------------------------
+
+# Grouping the document tree into Texinfo files. List of tuples
+# (source start file, target name, title, author,
+#  dir menu entry, description, category)
+texinfo_documents = [
+    (master_doc, 'packagetemplate', 'packagetemplate Documentation',
+     author, 'packagetemplate', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+
+# -- Options for Epub output -------------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+
+# The unique identifier of the text. This can be a ISBN number
+# or the project homepage.
+#
+# epub_identifier = ''
+
+# A unique identification for the text.
+#
+# epub_uid = ''
+
+# A list of files that should not be packed into the epub file.
+epub_exclude_files = ['search.html']
