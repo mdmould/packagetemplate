61413954e19af798150ad5efc5110cb124e2c2c2
MDM988 <mmould@star.sr.bham.ac.uk>
2020-05-27 14:27:15 +0100

    init imports

--- packagetemplate/__version__.py
+++ packagetemplate/__version__.py
@@ -9,3 +9,6 @@ __author__ = 'Matthew Mould'
 __author_email__ = 'mattdmould@gmail.com'
 __license__ = 'MIT'
 __url__ = 'https://github.com/mdmould/packagetemplate'
+
+__all__ = ['__title__', '__description__', '__version__', '__author__',
+           '__author_email__', '__license__', '__url__']
